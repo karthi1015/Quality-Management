2126b0455c159b98ded69d12349a59358d4b51c3
Yskert Schindel <yschindel@michaelisbruecke.lan>
2020-08-21 16:00:15 +0200

    Switched to class based testing

--- FloorsNamingConventions_script.py
+++ FloorsNamingConventions_script.py
@@ -79,189 +79,109 @@ ogs_none.SetProjectionLineColor(color_none2)
 
 # connect to revit model elements via FilteredElementCollector
 # collect all the floors
-floors = Fec(doc).OfCategory(Bic.OST_Floors).WhereElementIsNotElementType().ToElements()
-
-
-# make all lists to store floors in the process
-floors_gd = []
-floors_ba = []
-floors_gd_ba = []
-
-floors_gd_final = []
-floors_ba_final = []
-floors_gd_ba_final = []
-
-floors_none_final = []
-
-floor_ids = []
-
+elements = Fec(doc).OfCategory(Bic.OST_Floors).WhereElementIsNotElementType().ToElements()
 
 # regex to get material and thickness (_STB 25,5)
-# _[A-ZÄÖÜ]+\ [0-9]?[,]?[0-9]
-# regex to get total thickness (__52,5)
-# __[0-9]?[0-9]+?[,]?[0-9]
-
 regex_mat = re.compile('_[A-ZÄÖÜ]+\ [0-9]?[,]?[0-9]')
+# regex to get total thickness (__52,5)
 regex_total = re.compile('__\d+,?\d?\d?')
 regex_total_postfix = re.compile('__\d+,?\d?\d?_')
 
-
-# create main logic here..:
-
-# first rough seperation
-for floor in floors:
-    floor_id = floor.Id
-    floor_ids.append(floor_id)
-
-    # ask every door for its type name
-    floor_type = floor.Name
-
-    # seperate AW_ from IW_ from AW-FA_ from IW-FA_ from -none-
-    if floor_type.startswith("BA_"):
-        floors_ba.append(floor)
-
-    elif floor_type.startswith("GD_"):
-        floors_gd.append(floor)
-
-    elif floor_type.startswith("GD-BA_"):
-        floors_gd_ba.append(floor)
-
-    else:
-        floors_none_final.append(floor)
+# create class to contain all info about element
+class Element_info:
+    def __init__(self, none, gd, gdba, so, soba, ba, match, type_name, id):
+        self.none = none
+        self.gd = gd
+        self.gdba = gdba
+        self.so = so
+        self.soba = soba
+        self.ba = ba
+        self.match = match
+        self.type_name = type_name
+        self.id = id
 
 
-# check structural floors
-for floor in floors_gd:
+# has to run over lists: aw and iw
+def process_elements(element_list):
+    match_list = []
+    # check exterior elements
+    for el in element_list:
+        # ask every element for its type name
+        el_type = el.type_name
+        amount_of_mats = len(regex_mat.findall(el_type))
+        el_total = len(regex_total.findall(el_type))
+        el_total_postfix = len(regex_total_postfix.findall(el_type))
+        # check multi material elements:
+        if el_type.count("__") == 1 and amount_of_mats > 1 and el_total == 1:
+        # check for not free text at the end
+            if len(filter(None, re.split(regex_total, el_type))) == 1:
+                match_list.append(el)
+        # check for correct free text at the end
+            elif el_total_postfix == 1:
+                match_list.append(el)
+        # ckeck single material elements with no total thickness
+        elif el_type.count("__") == 0 and amount_of_mats == 1 and el_total == 0 and el.gdba == False and el.soba == False:
+            match_list.append(el)
+    return match_list
 
-    # ask every door for its type name
-    floor_type = floor.Name
-    amount_of_mats = len(regex_mat.findall(floor_type))
-    floor_total = len(regex_total.findall(floor_type))
-    floor_total_postfix = len(regex_total_postfix.findall(floor_type))
+element_ids = []
+elements_info = []
 
-    # check multi material floors:
-    if floor_type.count("__") == 1 and amount_of_mats > 1 and floor_total == 1:
+for element in elements:
+    # gather information and create objects of Element_info class
+    element_id = element.Id
+    element_ids.append(element_id)
+    element_type_name = element.Name
+    element_gd = False
+    element_gdba = False
+    element_so = False
+    element_soba = False
+    element_ba = False
+    if element_type_name.startswith("GD_"):
+        element_gd = True
+    elif element_type_name.startswith("GD-BA_"):
+        element_gdba = True
+    elif element_type_name.startswith("SO_"):
+        element_so = True
+    elif element_type_name.startswith("SO-BA_"):
+        element_soba = True
+    elif element_type_name.startswith("BA_"):
+        element_ba = True
+    element_info = Element_info(True, element_gd, element_gdba, element_so, element_soba, element_ba, False, element_type_name, element_id)
+    elements_info.append(element_info)
 
-    # check for not free text at the end
-        if len(filter(None, re.split(regex_total, floor_type))) == 1:
-            floors_gd_final.append(floor)
+elements_aw_iw = []
 
-    # check for correct free text at the end
-        elif floor_total_postfix == 1:
-            floors_gd_final.append(floor)
+for element in elements_info:
+    if element.gd or element.gdba or element.so or element.soba or element.ba:
+        elements_aw_iw.append(element)
 
-        else:
-            floors_none_final.append(floor)
+for el in process_elements(elements_aw_iw):
+    el.match = True
+    el.none = False
 
-    # ckeck single material floors with no total thickness
-    elif floor_type.count("__") == 0 and amount_of_mats == 1 and floor_total == 0:
-        floors_gd_final.append(floor)
-
-    else:
-        floors_none_final.append(floor)
-
-# check interior floors
-for floor in floors_ba:
-
-    # ask every door for its type name
-    floor_type = floor.Name
-    amount_of_mats = len(regex_mat.findall(floor_type))
-    floor_total = len(regex_total.findall(floor_type))
-    floor_total_postfix = len(regex_total_postfix.findall(floor_type))
-
-    #print("-"*15)
-    #print(floor_type)
-    #print(amount_of_mats)
-    #print(floor_total)
-
-    # check multi material floors:
-    if floor_type.count("__") == 1 and amount_of_mats > 1 and floor_total == 1:
-
-# check for not free text at the end
-        if len(filter(None, re.split(regex_total, floor_type))) == 1:
-            floors_ba_final.append(floor)
-
-    # check for correct free text at the end
-        elif floor_total_postfix == 1:
-            floors_ba_final.append(floor)
-
-        else:
-            floors_none_final.append(floor)
-
-    # ckeck single material floors with no total thickness
-    elif floor_type.count("__") == 0 and amount_of_mats == 1 and floor_total == 0:
-        floors_ba_final.append(floor)
-
-    else:
-        floors_none_final.append(floor)
-
-# check combined floors
-for floor in floors_gd_ba:
-
-    # ask every door for its type name
-    floor_type = floor.Name
-    amount_of_mats = len(regex_mat.findall(floor_type))
-    floor_total = len(regex_total.findall(floor_type))
-    floor_total_postfix = len(regex_total_postfix.findall(floor_type))
-
-    #print("-"*15)
-    #print(floor_type)
-    #print(amount_of_mats)
-    #print(floor_total)
-
-    # check multi material floors:
-    if floor_type.count("__") == 1 and amount_of_mats > 1 and floor_total == 1:
-
-# check for not free text at the end
-        if len(filter(None, re.split(regex_total, floor_type))) == 1:
-            floors_gd_ba_final.append(floor)
-
-    # check for correct free text at the end
-        elif floor_total_postfix == 1:
-            floors_gd_ba_final.append(floor)
-
-        else:
-            floors_none_final.append(floor)
-
-    # ckeck single material floors with no total thickness
-    elif floor_type.count("__") == 0 and amount_of_mats == 1 and floor_total == 0:
-        floors_gd_ba_final.append(floor)
-
-    else:
-        floors_none_final.append(floor)
+col1 = List[ElementId](element_ids)
 
 
-# visualize incorrect floors
-#print("Wrong naming:")
-#for floor in floors_none_final:
-#    print(floor.Name)
-
-
-col1 = List[ElementId](floor_ids)
-
-# entering a transaction to modify the revit model database
+# entering a transaction to modify the revit model database -------------------
 # start transaction
-tx = Transaction(doc, "check type names of floors")
+tx = Transaction(doc, "check type names of elements")
 tx.Start()
 
 doc.ActiveView.IsolateElementsTemporary(col1)
 
 # set graphical overrides
-for floor in floors_none_final:
-    doc.ActiveView.SetElementOverrides((floor.Id), ogs_none)
-
-for floor in floors_gd_final:
-    doc.ActiveView.SetElementOverrides((floor.Id), ogs_gd)
-
-for floor in floors_ba_final:
-    doc.ActiveView.SetElementOverrides((floor.Id), ogs_ba)
-
-for floor in floors_gd_ba_final:
-    doc.ActiveView.SetElementOverrides((floor.Id), ogs_gd_ba)
-
+for element in elements_info:
+    if element.match and (element.gd or element.so):
+        doc.ActiveView.SetElementOverrides((element.id), ogs_gd)
+    elif element.match and (element.gdba or element.soba):
+        doc.ActiveView.SetElementOverrides((element.id), ogs_gd_ba)
+    elif element.match and element.ba:
+        doc.ActiveView.SetElementOverrides((element.id), ogs_ba)
+    if element.none:
+        doc.ActiveView.SetElementOverrides((element.id), ogs_none)
 
 
 # commit the changes to the revit model database
 # end transaction
 tx.Commit()
-#print("successfully changed model")
